72846d996de92170795ea640ad1e5f0f3c60ea85
Vishnu Kumar <61048749+VISHNU-KUMAR7@users.noreply.github.com>
2023-08-27 23:36:43 +0530

    Update store.js
    Added a comment on store.js as it is using middleware and store function to call reducer and Sagar.

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -4,7 +4,9 @@ import createSagaMiddleware from "redux-saga";
 import rootSaga from "./saga/rootSaga";
 // const dummyReducer = () => {
 //   return 100;
-// };
+// }
+// calls reducer, saga file
+// use middleware and configurestore
 const sagaMiddleware = createSagaMiddleware();
 const store = configureStore({
   reducer: rootReducer,
